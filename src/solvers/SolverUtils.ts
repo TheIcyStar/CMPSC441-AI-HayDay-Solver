eff896cb6ff16c4d015db2dad03c5876a0804f00
Cheney <911125cheney@gmail.com>
2025-12-08 23:00:32 -0500

    Remove unused variable to fix build

--- src/solvers/SolverUtils.ts
+++ src/solvers/SolverUtils.ts
@@ -6,7 +6,7 @@ import type { SolutionStep } from "../typedefs/SolverTypes"
  * Returns a game state with layered actions
  */
 export function getLayeredGameState(initial: GameState, layers: SolutionStep[]): GameState {
-  let _stateData: GameState = JSON.parse(JSON.stringify(initial))
+
 
   for (const step of layers) {
     for (const _subtractingItem of step.newQueueItems) {
